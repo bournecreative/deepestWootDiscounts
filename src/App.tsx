076fefdff684b919bc3d914b784c24cba55ab17e
Christian <bournecreative@gmail.com>
2023-12-07 21:57:34 -0600

    sort returned data based on maxSavings, add property to item

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,54 @@
 import { useEffect, useState } from 'react';
 import { fetchApi } from './api';
 
+interface WootItem {
+  Categories: [];
+  Condition?: null;
+  EndDate: string;
+  FormatUrl: string;
+  IsAvailableOnMobileOnly: boolean;
+  isFeatured: boolean;
+  isFulfilledByAmazon: boolean;
+  isSoldOut: boolean;
+  isWootOut: boolean;
+  ListPrice: { Minimum: number; Maximum: number } | null;
+  OfferOd: string;
+  Photo: string;
+  SalePrice: { Minimum: number; Maximum: number };
+  Site: string;
+  Slug: null;
+  StartDate: string;
+  Subtitle: null;
+  Title: string;
+  Url: string;
+  Savings?: number;
+}
+
 function App() {
   const [items, setItems] = useState();
 
   const getData = async () => {
     const data = await fetchApi();
-
-    setItems(data);
+    console.log(data.Items);
+    const filtered = data.Items.filter((i: WootItem) => {
+      return i.ListPrice !== null;
+    }).sort((a: WootItem, b: WootItem) => {
+      if (a.ListPrice && b.ListPrice) {
+        const aSavings = a.ListPrice.Minimum - a.SalePrice.Minimum;
+        const bSavings = b.ListPrice.Minimum - b.SalePrice.Minimum;
+        a.Savings = aSavings;
+        b.Savings = bSavings;
+        return aSavings - bSavings;
+      }
+    });
+    setItems(filtered);
   };
 
   useEffect(() => {
     getData();
   }, []);
 
-  useEffect(() => {
-    console.log('items', items);
-  }, [items]);
+  useEffect(() => {}, [items]);
 
   return <>Get woot api data to pupulate from backend</>;
 }

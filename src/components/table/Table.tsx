62c120df5150e3b7555334012d6864bfe0228304
Christian <bournecreative@gmail.com>
2023-12-08 16:49:11 -0600

    added heading to table describing columns

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -10,11 +10,20 @@ export const TableData: React.FC<TableDataProps> = ({ items }) => {
     <div className="table_data">
       {items.length ? (
         <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Deal Description</th>
+              <th>List Price</th>
+              <th>Savings (based on sales price)</th>
+              <th>Woot link</th>
+            </tr>
+          </thead>
           <tbody>
             {items.map((item: WootItem, index) => {
               return (
                 <tr key={item.OfferId}>
-                  <td>{index}</td>
+                  <td>{index + 1}</td>
                   <td>{item.Title}</td>
                   <td>{item.ListPrice.Minimum}</td>
                   <td>{item.Savings}</td>

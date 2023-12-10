074b8fd79c9de6fd2852caddf0432e414887ce12
Christian <bournecreative@gmail.com>
2023-12-10 14:29:16 -0600

    styling table

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -12,23 +12,27 @@ export const TableData: React.FC<TableDataProps> = ({ items }) => {
         <table className="table">
           <thead className="table_header">
             <tr className="table_row">
-              <th>#</th>
-              <th>Deal Description</th>
-              <th>List Price</th>
-              <th>Savings (based on sales price)</th>
-              <th>Woot link</th>
+              <th className="header_cell">Rank</th>
+              <th className="header_cell">Product Description</th>
+              <th className="header_cell">List Price</th>
+              <th className="header_cell">Sale Price</th>
+              <th className="header_cell">Savings</th>
+              <th className="header_cell">Link</th>
             </tr>
           </thead>
-          <tbody>
+          <tbody className="table_body">
             {items.map((item: WootItem, index) => {
               return (
                 <tr className="table_row" key={item.OfferId}>
-                  <td>{index + 1}</td>
-                  <td>{item.Title}</td>
-                  <td>{item.ListPrice.Minimum}</td>
-                  <td>{item.Savings}</td>
-                  <td>
-                    <a href={item.Url}>Link</a>
+                  <td className="body_cell">{index + 1}</td>
+                  <td className="body_cell">{item.Title}</td>
+                  <td className="body_cell">${item.ListPrice.Minimum}</td>
+                  <td className="body_cell">${item.SalePrice.Minimum}</td>
+                  <td className="body_cell">${item.Savings}</td>
+                  <td className="body_cell">
+                    <a className="body_link" href={item.Url}>
+                      Go to deal
+                    </a>
                   </td>
                 </tr>
               );
